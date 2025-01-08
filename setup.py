f40c880339e914269ba1fb473ccb7de2b8f88d68
rafaelporfiriobarros <rafaporfirio.barros@gmail.com>
2025-01-08 19:49:32 -0300

    subindo setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    esta função retornará a lista de requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+
+
+
+
+
+setup(
+name='credit_risk_nubank',
+version='0.0.1',
+author='Rafael Porfirio',
+author_email='rafaporfirio.barros@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
+
